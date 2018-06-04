421164a1632cabdd380c0bad66a9f0eec1df3bcb
sranjani95 <sranjani95@gmail.com>
2018-06-04 22:33:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,118 +14,118 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
+  var fulfillmentText =
+    req.body.queryResult &&
+    req.body.queryResult.parameters &&
+    req.body.queryResult.parameters.echoText
+      ? req.body.queryResult.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    speech: speech,
-    displayText: speech,
+    fulfillmentText: fulfillmentText,
+    //displayText: fulfillmentText,
     source: "webhook-echo-sample"
   });
 });
 
 restService.post("/audio", function(req, res) {
-  var speech = "";
-  switch (req.body.result.parameters.AudioSample.toLowerCase()) {
-    //Speech Synthesis Markup Language 
+  var fulfillmentText = "";
+  switch (req.body.queryResult.parameters.AudioSample.toLowerCase()) {
+    //fulfillmentText Synthesis Markup Language 
     case "music one":
-      speech =
+      fulfillmentText =
         '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
       break;
     case "music two":
-      speech =
+      fulfillmentText =
         '<speak><audio clipBegin="1s" clipEnd="3s" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
       break;
     case "music three":
-      speech =
+      fulfillmentText =
         '<speak><audio repeatCount="2" soundLevel="-15db" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
       break;
     case "music four":
-      speech =
+      fulfillmentText =
         '<speak><audio speed="200%" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
       break;
     case "music five":
-      speech =
+      fulfillmentText =
         '<audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio>';
       break;
     case "delay":
-      speech =
+      fulfillmentText =
         '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
       break;
-    //https://www.w3.org/TR/speech-synthesis/#S3.2.3
+    //https://www.w3.org/TR/fulfillmentText-synthesis/#S3.2.3
     case "cardinal":
-      speech = '<speak><say-as interpret-as="cardinal">12345</say-as></speak>';
+      fulfillmentText = '<speak><say-as interpret-as="cardinal">12345</say-as></speak>';
       break;
     case "ordinal":
-      speech =
+      fulfillmentText =
         '<speak>I stood <say-as interpret-as="ordinal">10</say-as> in the class exams.</speak>';
       break;
     case "characters":
-      speech =
+      fulfillmentText =
         '<speak>Hello is spelled as <say-as interpret-as="characters">Hello</say-as></speak>';
       break;
     case "fraction":
-      speech =
+      fulfillmentText =
         '<speak>Rather than saying 24+3/4, I should say <say-as interpret-as="fraction">24+3/4</say-as></speak>';
       break;
     case "bleep":
-      speech =
+      fulfillmentText =
         '<speak>I do not want to say <say-as interpret-as="bleep">F&%$#</say-as> word</speak>';
       break;
     case "unit":
-      speech =
+      fulfillmentText =
         '<speak>This road is <say-as interpret-as="unit">50 foot</say-as> wide</speak>';
       break;
     case "verbatim":
-      speech =
+      fulfillmentText =
         '<speak>You spell HELLO as <say-as interpret-as="verbatim">hello</say-as></speak>';
       break;
     case "date one":
-      speech =
+      fulfillmentText =
         '<speak>Today is <say-as interpret-as="date" format="yyyymmdd" detail="1">2017-12-16</say-as></speak>';
       break;
     case "date two":
-      speech =
+      fulfillmentText =
         '<speak>Today is <say-as interpret-as="date" format="dm" detail="1">16-12</say-as></speak>';
       break;
     case "date three":
-      speech =
+      fulfillmentText =
         '<speak>Today is <say-as interpret-as="date" format="dmy" detail="1">16-12-2017</say-as></speak>';
       break;
     case "time":
-      speech =
+      fulfillmentText =
         '<speak>It is <say-as interpret-as="time" format="hms12">2:30pm</say-as> now</speak>';
       break;
     case "telephone one":
-      speech =
+      fulfillmentText =
         '<speak><say-as interpret-as="telephone" format="91">09012345678</say-as> </speak>';
       break;
     case "telephone two":
-      speech =
+      fulfillmentText =
         '<speak><say-as interpret-as="telephone" format="1">(781) 771-7777</say-as> </speak>';
       break;
     // https://www.w3.org/TR/2005/NOTE-ssml-sayas-20050526/#S3.3
     case "alternate":
-      speech =
+      fulfillmentText =
         '<speak>IPL stands for <sub alias="indian premier league">IPL</sub></speak>';
       break;
   }
   return res.json({
-    speech: speech,
-    displayText: speech,
+    fulfillmentText: fulfillmentText,
+    //displayText: fulfillmentText,
     source: "webhook-echo-sample"
   });
 });
 
 restService.post("/video", function(req, res) {
   return res.json({
-    speech:
+    fulfillmentText:
       '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-    displayText:
-      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+    //displayText:
+      //'<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
     source: "webhook-echo-sample"
   });
 });
@@ -186,8 +186,8 @@ restService.post("/slack-test", function(req, res) {
     ]
   };
   return res.json({
-    speech: "speech",
-    displayText: "speech",
+    fulfillmentText: "fulfillmentText",
+   // displayText: "fulfillmentText",
     source: "webhook-echo-sample",
     data: {
       slack: slack_message
