b17a7f5f1ad86555c581938345abb6e2c422abef
sranjani95 <sranjani95@gmail.com>
2018-06-04 23:43:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const cheerio = require('cheerio');
+var request = require('request');
+var fs = require('fs');
 
 const restService = express();
 
@@ -13,21 +16,22 @@ restService.use(
 
 restService.use(bodyParser.json());
 
+//scrapping
+
 restService.post("/echo", function(req, res) {
-  var fulfillmentText =
-    req.body.queryResult &&
-    req.body.queryResult.parameters &&
-    req.body.queryResult.parameters.echoText
-      ? req.body.queryResult.parameters.echoText
+	var url = "https://www.capgemini.com/our-company/"
+    request(url,function(err,resp,body){
+	var $ = cheerio.load(body);
+	var details = $('.card__media-overlapping__text');
+	var fulfillmentText = details.text() ? req.body.queryResult.parameters.echoText
       : "Seems like some problem. Speak again.";
+
   return res.json({
     fulfillmentText: fulfillmentText,
     //displayText: fulfillmentText,
     source: "webhook-echo-sample"
   });
 });
-
-
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
