c2e2850ea55a23e7ca52e39c41d93919c3e8cdcc
sranjani95 <sranjani95@gmail.com>
2018-06-05 12:56:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,28 +16,24 @@ restService.use(
 restService.use(bodyParser.json());
 
 
-
 restService.post("/echo", function(req, res) {
-        global.fulfillmentText = req.body.queryResult.intent.displayName;
-	var url = "https://www.capgemini.com/our-company/"
-         request(url,function(err,resp,body){
-	    var $ = cheerio.load(body);
-	    var details = $('.card__media-overlapping__text');
-         //global.fulfillmentText = details.text() + intentName;
-	//console.log(detailstext);
+  global.intentName = req.body.queryResult.intent.displayName;
+
+  if(intentName =="Echo"){
+  var url = "https://www.capgemini.com/our-company/"
+  request(url,function(err,resp,body){
+    var $ = cheerio.load(body);
+    var details = $('.card__media-overlapping__text');
+    global.fulfillmentText = details.text() ;
+  
 });
-  // var fulfillmentText =
-  //   req.body.queryResult &&
-  //   req.body.queryResult.parameters &&
-  //   req.body.queryResult.parameters.echoText
-  //     ? req.body.queryResult.parameters.echoText
-  //     : "Seems like some problem. Speak again.";
+  
   return res.json({
     fulfillmentText: fulfillmentText,
-    //displayText: fulfillmentText,
     source: "webhook-echo-sample"
   });
 });
+}
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
